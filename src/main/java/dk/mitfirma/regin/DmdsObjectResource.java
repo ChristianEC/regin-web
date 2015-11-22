db86e241c074d54d234ccb53c440847e17a744cb
Christian Ertmann-Christiansen <chec@kb.dk>
2015-11-22 20:03:15 +0100

    Made code generic to handle all object types. Renamed resource classes DmdsObject... as a consequence. And some other stuff.

---
+++ src/main/java/dk/mitfirma/regin/DmdsObjectResource.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.mitfirma.regin;
+
+import dk.mitfirma.dmds.DmdsObject;
+import dk.mitfirma.dmds.DmdsObjectEJB;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.DELETE;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+/**
+ * REST Web Service
+ *
+ * @author christian
+ */
+@RequestScoped
+public class DmdsObjectResource {
+
+    private final String id;
+    private final DmdsObjectEJB dmdsService;
+    private final UriInfo uriInfo;
+    private DmdsObject object;
+    
+    /**
+     * Creates a new instance of GenstandResource
+     * @param id
+     * @param r
+     */
+    public DmdsObjectResource(String id, DmdsObjectsResource r) {
+        this.id = id;
+        uriInfo = r.uriInfo;
+        dmdsService = r.dmdsService;
+        object = dmdsService.findById(id);
+    }
+
+    /**
+     * Get instance of the DmdsObjectResource
+     * @param id
+     * @param r
+     * @return 
+     */
+    public static DmdsObjectResource getInstance(String id, DmdsObjectsResource r) {
+        return new DmdsObjectResource(id, r);
+    }
+
+    /**
+     * Retrieves representation of an instance of dk.mitfirma.regin.DmdsObjectResource
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces("application/xml")
+    public String getXml() throws NotFoundException {   
+        if (object == null) throw new NotFoundException();
+        return object.toXML();
+    }
+
+    /**
+     * PUT method for updating or creating an instance of DmdsObjectResource
+     * @param content representation for the resource
+     * @return an HTTP response with content of the updated or created resource.
+     */
+    @PUT
+    @Consumes("application/xml")
+    @Produces("application/xml")
+    public Response putXml(String content) {
+        Response response;
+        
+        if (object == null) {
+            object = dmdsService.newObject().fromXML(content);
+            object.setId(id);
+            dmdsService.create(object);
+            response = Response.created(uriInfo.getRequestUri()).entity(object.toXML()).build();
+        } else {
+            object = dmdsService.update(object.fromXML(content));
+            response = Response.ok(object.toXML()).build();
+       }
+       return response;
+    }
+
+    /**
+     * DELETE method for resource DmdsObjectResource
+     * @return an HTTP response with content of the deleted resource
+     */
+    @DELETE
+    @Produces("application/xml")
+    public String delete() throws NotFoundException {
+        object = dmdsService.delete(id);
+        if (object == null)
+            throw new NotFoundException();
+        else
+            return object.toXML();
+    }
+}
---
+++ src/main/java/dk/mitfirma/regin/DmdsObjectsResource.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.mitfirma.regin;
+
+import dk.mitfirma.dmds.DmdsObject;
+import dk.mitfirma.dmds.DmdsObjectEJB;
+import dk.mitfirma.dmds.FilEJB;
+import dk.mitfirma.dmds.GenstandEJB;
+import dk.mitfirma.dmds.SagEJB;
+import java.util.List;
+import static java.util.stream.Collectors.joining;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.core.Response;
+
+/**
+ * REST Web Service
+ *
+ * @author christian
+ */
+@Path("/{objType}")
+@RequestScoped
+public class DmdsObjectsResource {
+
+    @Context
+    UriInfo uriInfo;
+    @PathParam("objType")
+    private String objType;
+    @EJB(name = "SagEJB")
+    private SagEJB sagEJB;
+    @EJB(name = "GenstandEJB")
+    private GenstandEJB genstandEJB;
+    @EJB(name = "FilEJB")
+    private FilEJB filEJB;
+    DmdsObjectEJB dmdsService;
+
+    /**
+     * Creates a new instance of GenstandsResource
+     */
+    public DmdsObjectsResource() {}
+    
+    @PostConstruct
+    private void postConstruct() {
+        switch (objType) {
+            case "genstand": dmdsService = genstandEJB; break;
+            case "sag": dmdsService = sagEJB; break;
+            case "fil": dmdsService = filEJB; break;
+            default: throw new NotFoundException();
+        }
+    }
+    
+    /**
+     * Retrieves representation of an instance of dk.mitfirma.regin.DmdsObjectsResource
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces("application/xml")
+    public String getXml() {
+        String context = uriInfo.getRequestUri().toString();
+        String prefix = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<xhtml><head>\n<link href='" + context + "/";
+        String delimiter = "' />\n<link href='" + context + "/";
+        String suffix = "' />\n</head><body /></xhtml>";
+        List<DmdsObject> L = dmdsService.findAll();
+        return L.stream().map(o -> o.getId().toString()).collect(joining(delimiter, prefix, suffix));
+    }
+
+    /**
+     * POST method for creating an instance of DmdsObjectResource
+     * @param content representation for the new resource
+     * @return an HTTP response with content of the created resource
+     */
+    @POST
+    @Consumes("application/xml")
+    @Produces("application/xml")
+    public Response postXml(String content) {
+        DmdsObject o = dmdsService.newObject().fromXML(content);
+        dmdsService.create(o);
+        return Response.created(uriInfo.getRequestUriBuilder().path(o.getId().toString()).build()).entity(o.toXML()).build();
+    }
+
+    /**
+     * Sub-resource locator method for {id}
+     * @param id
+     * @return 
+     */
+    @Path("{id}")
+    public DmdsObjectResource getGenstandResource(@PathParam("id") String id) {
+        return DmdsObjectResource.getInstance(id, this);
+    }
+}
