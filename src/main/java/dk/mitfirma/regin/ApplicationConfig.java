3487af6a7be849345166475ad6bf1bfba69210fd
Christian Ertmann-Christiansen <chec@kb.dk>
2015-11-25 17:42:19 +0100

    getXml now returns valid xml for zero-length lists of resources.

--- src/main/java/dk/mitfirma/regin/ApplicationConfig.java
+++ src/main/java/dk/mitfirma/regin/ApplicationConfig.java
@@ -29,7 +29,6 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(dk.mitfirma.regin.DmdsObjectResource.class);
         resources.add(dk.mitfirma.regin.DmdsObjectsResource.class);
     }
     
